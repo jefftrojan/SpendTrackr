4c2e696a4ff3e98a218c83fa1242e2bf0d36d8cb
jefftrojan <j.dauda@alustudent.com>
2023-08-14 15:41:18 +0200

    created components dir

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Stack } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container class='my-4'>
+      <Stack  direction='horizontal' gap='2' className='mb-4'>
+        <h1 className='me-auto'> Budgets</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='secondary'>Add Expense</Button>
+
+      </Stack>
+       <div 
+       style={
+        {
+          display: 'grid', 
+          gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start'
+        }
+       }
+       
+       
+       >
+
+       </div>
+    </Container>
   );
 }
 
